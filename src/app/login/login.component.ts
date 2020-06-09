4915bd782341dbcfba206562cce41719a1e94bed
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-09 21:10:44 +0530

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,27 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  loginUser() {
+    this.authService.login({
+      username: 'General User',
+      email: 'user@domain.com',
+      role: ['user'],
+    });
+  }
+
+  loginAdmin() {
+    this.authService.login({
+      username: 'Admin User',
+      email: 'admin@domain.com',
+      role: ['user', 'admin'],
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
