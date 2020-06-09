4c8d0c0d8f6c2ffcbc5a21cfcf9b33f969f91b58
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-09 21:15:21 +0530

    done

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  // inject the auth service here
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    // check if the user observable has truthy object
+    return this.authService.user$.pipe(
+      map((v) => {
+        if (v) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
+}
